4bf62d3a7bcfc4436f0f443fd38b65873c697a20
Evan Fitzgerald Griffith <evfitzgriff@Evans-MacBook-Pro.local>
2025-11-02 17:38:37 -0500

    Initial commit — OHPAL Lab site

---
+++ .next/static/webpack/app/page.7d350a13b830f51b.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fevfitzgriff%2FDesktop%2FOHPAL%2Fohpal-living-lab-site%202%2Fapp%2Fcomponents%2FContactForm.tsx%22%2C%22ids%22%3A%5B%22ContactForm%22%5D%7D&server=false!":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fevfitzgriff%2FDesktop%2FOHPAL%2Fohpal-living-lab-site%202%2Fapp%2Fcomponents%2FContactForm.tsx%22%2C%22ids%22%3A%5B%22ContactForm%22%5D%7D&server=false! ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/components/ContactForm.tsx */ \"(app-pages-browser)/./app/components/ContactForm.tsx\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyJTJGVXNlcnMlMkZldmZpdHpncmlmZiUyRkRlc2t0b3AlMkZPSFBBTCUyRm9ocGFsLWxpdmluZy1sYWItc2l0ZSUyMDIlMkZhcHAlMkZjb21wb25lbnRzJTJGQ29udGFjdEZvcm0udHN4JTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyQ29udGFjdEZvcm0lMjIlNUQlN0Qmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSwwTEFBNkoiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz84MjhkIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiLCB3ZWJwYWNrRXhwb3J0czogW1wiQ29udGFjdEZvcm1cIl0gKi8gXCIvVXNlcnMvZXZmaXR6Z3JpZmYvRGVza3RvcC9PSFBBTC9vaHBhbC1saXZpbmctbGFiLXNpdGUgMi9hcHAvY29tcG9uZW50cy9Db250YWN0Rm9ybS50c3hcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2FUsers%2Fevfitzgriff%2FDesktop%2FOHPAL%2Fohpal-living-lab-site%202%2Fapp%2Fcomponents%2FContactForm.tsx%22%2C%22ids%22%3A%5B%22ContactForm%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
